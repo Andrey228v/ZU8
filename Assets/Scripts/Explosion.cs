ebd91088417b8c98d014e2b4ef8d837a1b6e3379
Andrey <gysev003@mail.ru>
2024-07-07 19:59:02 +0300

    V8

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 [RequireComponent(typeof(Figure))]
 public class Explosion : MonoBehaviour
 {
+    [SerializeField] private float _radius;
+    [SerializeField] private float _force;
+
     private Figure _figure;
     private int _minRandomParticles = 2;
     private int _maxRandomParticles = 7;
@@ -16,6 +19,23 @@ public class Explosion : MonoBehaviour
         if (_figure.IsDivision())
         {
             CreateParticleExplosion(figurCreater, explosionObject);
+        }
+        else
+        {
+            Collider[] overlappedColliders = Physics.OverlapSphere(explosionObject.transform.position, _radius);
+
+            float scaleX = explosionObject.transform.localScale.x;
+
+            for (int i = 0; i < overlappedColliders.Length; i++)
+            {
+                Rigidbody rigidbody = overlappedColliders[i].attachedRigidbody;
+
+                if(rigidbody != null )
+                {
+                    rigidbody.AddExplosionForce(_force, explosionObject.transform.position/ scaleX, _radius/ scaleX);
+                }
+            }
+
         }
 
         Destroy(explosionObject);
